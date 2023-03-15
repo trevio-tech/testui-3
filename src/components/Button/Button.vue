7da80c526a7aedfe0978f0a944e791cf594f5d4f
Denis <zabavnikov.com@gmail.com>
2023-03-15 09:54:55 +0700

    wip

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,3 +1,75 @@
 <template>
-  <button class="bg-orange-200 p-2">КНОПКА</button>
+  <button
+    :class="classes"
+    :type="type"
+    :disabled="disabled || loading"
+    v-ripple
+    @click="onClick($event)"
+  >
+    <Loader v-if="loading" />
+    <span :style="{visibility: loading ? 'hidden' : 'visible'}"><slot></slot></span>
+    <slot v-if="hasAppendSlot" name="append"></slot>
+  </button>
 </template>
+
+<script>
+import Loader from '../Loader/Loader.vue'
+
+export default {
+  name: 'Button',
+  components: {
+    Loader
+  },
+  emits: ['click'],
+  setup(_, { emit }) {
+    const onClick = (event) => emit('click', event)
+
+    return {
+      onClick
+    }
+  },
+  props: {
+    variant: {
+      type: String,
+      default: 'primary',
+      validator(type) {
+        return ['primary', 'secondary', 'danger', 'success'].includes(type)
+      },
+    },
+    type: {
+      type: String,
+      default: 'button',
+      validator(type) {
+        return ['button', 'submit', 'reset'].includes(type)
+      },
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  computed: {
+    hasAppendSlot() {
+      return !!this.$slots['append'];
+    },
+    classes() {
+      const variants = {
+        primary:    'text-white bg-primary enabled:hover:bg-primary-dark focus:ring-primary-light focus:ring-4',
+        secondary:  'text-black border border-gray-lightest enabled:hover:bg-gray-lightest',
+        danger:     'text-white bg-red enabled:hover:bg-red-dark focus:ring-red-light focus:ring-4',
+        success:    'text-white bg-green enabled:hover:bg-green-dark focus:ring-green-light focus:ring-4',
+      };
+
+      return {
+        'relative font-medium rounded-lg text-sm px-5 h-[42px] focus:outline-none disabled:opacity-75 transition duration-100 ease-in-out"': true,
+        [variants[this.variant]]: true,
+        'flex items-center space-x-1': this.hasAppendSlot
+      }
+    },
+  },
+}
+</script>
---
+++ src/components/Loader/Loader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="loader-wrapper">
+    <div class="loader"></div>
+  </div>
+</template>
+
+<style>
+.loader-wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0,0,0,.24);
+  z-index: 100;
+}
+.loader,
+.loader:after {
+  border-radius: 50%;
+  width: 1rem;
+  height: 1rem;
+}
+.loader {
+  position: relative;
+  margin: auto;
+  text-indent: -9999em;
+  border-top: 4px solid rgba(255, 255, 255, 0.2);
+  border-right: 4px solid rgba(255, 255, 255, 0.2);
+  border-bottom: 4px solid rgba(255, 255, 255, 0.2);
+  border-left: 4px solid #ffffff;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation: load8 1.1s infinite linear;
+  animation: load8 1.1s infinite linear;
+}
+@-webkit-keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+</style>
