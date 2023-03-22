a0cc0be83e05bc79135d0f188249ec5c7efe038c
Denis <zabavnikov.com@gmail.com>
2023-03-22 10:40:51 +0700

    wip

---
+++ src/components/ContentList/ContentList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <slot :items="items" :isMore="isMore" :onFetch="getItems" />
+  </div>
+</template>
+
+<script setup>
+import { shallowRef } from 'vue'
+import { useQuery } from '../../index'
+
+const props = defineProps({
+  travelId: {
+    type: [String, Number]
+  },
+  userId: {
+    type: [String, Number]
+  },
+  limit: {
+    type: Number,
+    default: 10
+  },
+  query: {
+    type: String,
+    required: true
+  },
+})
+
+const isLoading = shallowRef(false)
+const items = shallowRef([])
+const page = shallowRef(1)
+const isMore = shallowRef(true)
+
+const getItems = async () => {
+  try {
+    if (isLoading.value) {
+      return
+    }
+
+    isLoading.value = true
+
+    const { data: { userContent }} = await useQuery({
+      query: props.query,
+      variables: {
+        travel_id: props.travelId,
+        user_id: props.userId,
+        page: page.value,
+        limit: props.limit
+      }
+    })
+
+    isMore.value = userContent.length >= props.limit
+    items.value = [...items.value, ...userContent]
+    page.value++
+  } catch (error) {}
+  finally {
+    isLoading.value = false
+  }
+}
+
+await getItems()
+</script>
