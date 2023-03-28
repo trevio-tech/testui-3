816e96efe3f494f830582a341cb499f795e6920f
Denis <zabavnikov.com@gmail.com>
2023-03-28 15:29:11 +0700

    wip

--- src/components/Dropdown/DropdownItem.vue
+++ src/components/Dropdown/DropdownItem.vue
@@ -4,20 +4,21 @@
     :to="to"
     :type="as === 'button' ? 'button' : undefined"
     v-bind="$attrs"
-    class="flex items-center bg-white w-full text-left py-2 px-3 hover:bg-stone-100 cursor-pointer rounded-lg"
-    @click="$emit('click')"
+    class="relative overflow-hidden flex items-center bg-white w-full text-left py-2 px-3 hover:bg-stone-100 cursor-pointer rounded-lg"
+    @click="onClick"
   >
     <span v-if="hasPrependSlot" class="mr-3"><slot name="prepend" /></span>
     <span class="text-sm font-medium text-gray-700"><slot /></span>
     <span v-if="hasAppendSlot" class="ml-3"><slot name="append" /></span>
+    <Loader v-if="busy" />
   </Component>
 </template>
 
 <script setup>
 import { resolveComponent, computed, useSlots } from 'vue'
+import Loader from '../Loader/Loader.vue'
 
-defineEmits(['click'])
-
+const emits = defineEmits(['click'])
 const props = defineProps({
   as: {
     type: String,
@@ -25,6 +26,9 @@ const props = defineProps({
   },
   to: {
     type: [String, Object]
+  },
+  busy: {
+    type: Boolean
   }
 })
 
@@ -37,4 +41,12 @@ const component = computed(() => {
   if (props.to || props.as === 'NuxtLink') return resolveComponent('NuxtLink')
   return props.as
 })
+
+const onClick = (event) => {
+  if (props.busy) {
+    event.stopImmediatePropagation()
+  }
+
+  emits('click', event)
+}
 </script>
