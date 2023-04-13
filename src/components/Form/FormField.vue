781d320c3d50881febe23fc7953ae9b30e91ec84
Denis <zabavnikov.com@gmail.com>
2023-04-13 12:06:06 +0700

    fix

--- src/components/Form/FormField.vue
+++ src/components/Form/FormField.vue
@@ -1,14 +1,16 @@
 <template>
-  <Field :name="name" :rules="rules" v-slot="{ errorMessage }" as="div">
-    <label v-if="label" class="font-medium text-sm">{{ label }}<span v-if="required" class="text-red-400 ml-1">*</span></label>
-    <div class="mt-1">
-      <slot :hasError="errorMessage !== undefined" />
-    </div>
-    <p v-if="errorMessage" class="mt-1 text-red-500 font-medium text-xs">{{ errorMessage }}</p>
-    <p v-if="!!$slots.help && !errorMessage" class="mt-1 text-gray-500 font-medium text-xs">
-      <slot name="help"></slot>
-    </p>
-  </Field>
+  <div>
+    <Field :name="name" :rules="rules" v-slot="{ errorMessage }">
+      <label v-if="label" class="font-medium text-sm">{{ label }}<span v-if="required" class="text-red-400 ml-1">*</span></label>
+      <div class="mt-1">
+        <slot :hasError="errorMessage !== undefined" />
+      </div>
+      <p v-if="errorMessage" class="mt-1 text-red-500 font-medium text-xs">{{ errorMessage }}</p>
+      <p v-if="!!$slots.help && !errorMessage" class="mt-1 text-gray-500 font-medium text-xs">
+        <slot name="help"></slot>
+      </p>
+    </Field>
+  </div>
 </template>
 
 <script setup>
